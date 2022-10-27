f924bb0a48e07bb7f75e9b3aa4675de39d5ea6ed
ThibautNarivelo <34999909+ThibautNarivelo@users.noreply.github.com>
2022-10-27 10:24:46 +0200

    fix codemod

--- components/Masthome.tsx
+++ components/Masthome.tsx
@@ -15,7 +15,7 @@ export const Masthome: FC<MastHomeProps> = ({ designs }) => {
           key={design._id}
           className="h-full w-full flex flex-col items-center justify-center"
         >
-          <motion.div
+          {/* <motion.div
             initial={{
               opacity: 0,
             }}
@@ -32,9 +32,9 @@ export const Masthome: FC<MastHomeProps> = ({ designs }) => {
               src={urlFor(design.rhoumeTitle).url()}
               alt="Rhoume Title Design"
             />
-          </motion.div>
+          </motion.div> */}
 
-          <motion.div
+          {/* <motion.div
             initial={{
               opacity: 0,
             }}
@@ -48,7 +48,7 @@ export const Masthome: FC<MastHomeProps> = ({ designs }) => {
             className="w-[52vw] my-2 lg:w-[37vw] xl:[15vw] 2xl:w-[32vw] hover:blur-sm duration-700"
           >
             <img src={urlFor(design.forroomTitle).url()} alt="For_Room Title" />
-          </motion.div>
+          </motion.div> */}
 
           <motion.div
             initial={{
@@ -61,7 +61,7 @@ export const Masthome: FC<MastHomeProps> = ({ designs }) => {
               duration: 0.5,
             }}
             viewport={{ once: true }}
-            className=" w-[55vw] hover:w-[60vw] md:w-[55vw] hover:md:w-[60vw] lg:w-[40vw] hover:lg:w-[45vw] xl:w-[40vw] 2xl:w-[35vw]  hover:2xl:w-[40vw] hover:xl:w-[45vw] ease-in-out duration-700"
+            className=" w-[55vw] hover:w-[60vw] md:w-[55vw] hover:md:w-[60vw] lg:w-[40vw] hover:lg:w-[45vw] xl:w-[20vw] 2xl:w-[20vw] hover:2xl:w-[15vw] hover:xl:w-[25vw] ease-in-out duration-700"
           >
             <img src={urlFor(design.rhoumeDesign).url()} alt="Rhoume Design" />
           </motion.div>
