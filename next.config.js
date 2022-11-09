9aa3c1415f9fd1129b21b2adebc8eeb95e80f57e
ThibautNarivelo <34999909+ThibautNarivelo@users.noreply.github.com>
2022-11-09 13:23:54 +0100

    improve seo

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
-
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
     images: {
     domains: ['cdn.sanity.io'],
   },
-}
+})
